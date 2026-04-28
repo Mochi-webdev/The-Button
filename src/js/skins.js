a2fd40e7e8d34bd3f2b6d66683ee2f824d9245be
Lorentious <72303448+Lorentious@users.noreply.github.com>
2026-04-28 08:43:20 +0000

    Füge neue Button-Skins und deren Logik hinzu

---
+++ src/js/skins.js
@@ -0,0 +1,46 @@
+const skins = [
+  {
+    id: "Orange_Btn", 
+    btn_down: "ButtonCommon1-Down.png",
+    btn_up: "ButtonCommon1.png",
+    color: "orange",
+    cost: "10000",
+    cost_label: "10k",
+    currency: "Clicks",
+    tite: "Orange Button Skin",
+    buy_label: "Get an Orange Button Skin!"
+  },
+  {
+    id: "Blue_Btn", 
+    btn_down: "ButtonCommon2-Down.png",
+    btn_up: "ButtonCommon2.png",
+    color: "blue",
+    cost: "10000",
+    cost_label: "10k",
+    currency: "Clicks",
+    tite: "Blue Button Skin",
+    buy_label: "Get an Blue Button Skin!"
+  },
+  {
+    id: "Green_Btn", 
+    btn_down: "ButtonCommon3-Down.png",
+    btn_up: "ButtonCommon3.png",
+    color: "green",
+    cost: "10000",
+    cost_label: "10k",
+    currency: "Clicks",
+    tite: "Green Button Skin",
+    buy_label: "Get an Green Button Skin!"
+  },
+  {
+    id: "Purple_Btn", 
+    btn_down: "ButtonCommon4-Down.png",
+    btn_up: "ButtonCommon4.png",
+    color: "purple",
+    cost: "10000",
+    cost_label: "10k",
+    currency: "Clicks",
+    tite: "Purple Button Skin",
+    buy_label: "Get an Purple Button Skin!"
+  },
+];
