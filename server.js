b083f2c30fbb0b049ea7e85c9eee1c9aa8dd6947
Mochi-webdev <luke.niessen@outlook.com>
2026-04-29 11:16:28 +0000

    Supbase

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require('http');   // HTTP module (built-in)
+const fs = require('fs');       // File system module (built-in)
+const path = require('path');   // Path utilities module (built-in)
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient('iazmgaqynonfeaootwkh.supabase.co', 'your-publishable-key')
+
 
 const PORT = 3000;
 
+//Serve Website
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
      
